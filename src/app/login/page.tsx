dd47426ad9ea6596e5ed511e90cd23e9ac7dc380
tonypham90 <tuananh.gt@hotmail.com>
2023-12-07 16:04:56 +0700

    update page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import LoginForm from './form';
-
-// export default function LoginPage() {
-//   return (
-//     <section className="bg-ct-blue-600 min-h-screen pt-20">
-//       <div className="container mx-auto px-6 py-12 h-full flex justify-center items-center">
-//         <div className="md:w-8/12 lg:w-5/12 px-8 py-10 rounded-md">
-//           <LoginForm />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
 'use client';
 import Link from 'next/link';
 import React, { useEffect } from 'react';
