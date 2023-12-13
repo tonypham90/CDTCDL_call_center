d2ef9279a56feeb3cf6cf76ae1ec5a3d7bdd6f86
Anh Pham <tuananhdc08kt@gmail.com>
2023-12-13 08:31:48 +0700

    update login page

--- src/services/socket/orders.tsx
+++ src/services/socket/orders.tsx
@@ -3,16 +3,17 @@ import { useEffect, useState } from 'react';
 import ServiceOrder from 'services/ServiceOrder';
 import ServiceUser from 'services/ServiceUser';
 import { io } from 'socket.io-client';
+import {buttonGroup} from "@nextui-org/theme";
+import Link from "next/link";
+import {router} from "next/client";
 
 const SERVER_URL = process.env.API_SERVER || 'https://grabapi-192a6fe739cb.herokuapp.com'; // replace with your server URL
 let listOrder: ServiceOrder[] = [];
 const Order = () => {
   const [data, setData] = useState<IExistingOrder[]>([]);
 
-  for (let i = 0; i < data.length; i++) {
-    const order = new ServiceOrder(data[i]);
-    listOrder.push(order);
-  }
+
+
   useEffect(() => {
     const socket = io(SERVER_URL);
 
@@ -21,6 +22,11 @@ const Order = () => {
     socket.on('connectedOrders', (newData: IExistingOrder[]) => {
       setData(newData);
     });
+
+    for (let i = 0; i < data.length; i++) {
+      const order = new ServiceOrder(data[i]);
+      listOrder.push(order);
+    }
 
     // cleanup function will run when the component unmounts
     return () => {
@@ -40,11 +46,26 @@ const Order = () => {
         </thead>
         <tbody>
           {listOrder.map((row, index) => (
-            <tr key={index}>
-              <td>{row.passenger}</td>
-              <td>{row.column2}</td>
-              {/* Add more cells as needed */}
-            </tr>
+              <tr key={index}>
+                <td>{row.passenger?.name()}</td>
+                <td>{row.passenger?.phone()}</td>
+                <td>{row.order?.departure.address}</td>
+                <td>{row.order?.destination.address}</td>
+                <td>{row.driver?.name()}</td>
+                <td>{row.driver?.phone()}</td>
+                <td>{row.order?.status}</td>
+                <td>
+                    <button
+                        onClick={() => {
+                          router.push(`/order/${row.order?.id}`);
+                        }}
+                    >
+                        Detail
+                    </button>
+                </td>
+
+                {/* Add more cells as needed */}
+              </tr>
           ))}
         </tbody>
       </table>
@@ -52,4 +73,4 @@ const Order = () => {
   );
 };
 
-export default MyComponent;
+export default Order;
